3768ab35a6a65c945b07739b2836e25517061971
Willow-Bot151 <willowhart151@gmail.com>
2024-08-28 20:03:59 +0100

    fixed rounding, still an unknown error in math

--- credit_card_balance_main.py
+++ credit_card_balance_main.py
@@ -1,13 +1,18 @@
+import math
+
 def credit_card_func(outstanding_balance,yearly_charge,monthly_repay):
     new_outstanding_balance = outstanding_balance
     for i in range(12):
-        min_pay = new_outstanding_balance * monthly_repay
-        print(min_pay)
-        balance_after_min_pay = new_outstanding_balance - min_pay
-        print(balance_after_min_pay)
-        percent_charge = (yearly_charge/12)*balance_after_min_pay
-        print(percent_charge)
-        new_outstanding_balance = balance_after_min_pay + percent_charge
+        print(f"Month {i+1}")
         print(new_outstanding_balance)
+        min_pay = (math.ceil(new_outstanding_balance * monthly_repay * 100)) / 100
+        print(min_pay)
+        balance_after_min_pay = round(new_outstanding_balance - min_pay,2)
+        print(balance_after_min_pay)
+        percent_charge = (math.ceil((yearly_charge/12)*balance_after_min_pay* 100)) / 100
+        print(percent_charge)
+        new_outstanding_balance = round(balance_after_min_pay + percent_charge,2)
+        print(new_outstanding_balance)
+        print("\n")
     string_out = f"Outstanding Balance: {new_outstanding_balance:.2f}"
     return string_out
